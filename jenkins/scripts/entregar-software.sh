92810752db1f6fd871ac14ad871135859ae67083
Julio Cesar Mejia Teran <julio@i7.scz.bo>
2018-10-02 02:48:25 -0400

    otra actualización

--- jenkins/scripts/entregar-software.sh
+++ jenkins/scripts/entregar-software.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd jenkins/scripts
 . ./env
-#scp -r . $usuario@$servidor:personas/. 
+#sshpass -p $clave scp -r . $usuario@$servidor:personas/. 
 echo "$usuario@$servidor:"
 echo "Entregar software OK"
 exit 0
