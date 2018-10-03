0e58021b9c08766faa3aff7eae5be91d9cdd75c5
Julio Cesar Mejia Teran <julio@i7.scz.bo>
2018-10-03 02:32:28 -0400

    test unitarios

--- agenda/tests.py
+++ agenda/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
-
+from agenda.models import Contactos
 # Create your tests here.
+
+class ContactosTestCase(TestCase):
+    def setUp(self):
+        Contactos.objects.create(nombre_text="Juan3", apellido_text="Perez3", nacimiento_date="1995-07-01 10:10:10", celular_text="12345678")
+        #Contactos.objects.all()
+        pass
+    
+    def test_contactos_tiene_nombre_apellido(self):
+        """Un contacto debe tener nombre y apellido"""
+        cont = Contactos.objects.get(nombre_text="Juan3")
+        self.assertEqual(str(cont), 'Juan3 Perez3')
